8603539f96b9f222d8ec804ac434023a069e70ea
th3malcode <th3mal@outlook.com>
2025-05-24 20:40:34 +1000

    explosion not affecting any blocks handling

--- behavior_packs/logExplosions/scripts/main.js
+++ behavior_packs/logExplosions/scripts/main.js
@@ -1,6 +1,6 @@
 // https://jaylydev.github.io/scriptapi-docs/latest/classes/_minecraft_server.WorldAfterEvents.html#explosion
 //import { world, system, Player, Entity } from '@minecraft/server';
-import { world } from '@minecraft/server';
+import { world, system } from '@minecraft/server';
 import { typeIds } from './typeIds.js';
 
 function logEvent(eve, typ,  nam, dim, loc) {
@@ -12,10 +12,10 @@ function logEvent(eve, typ,  nam, dim, loc) {
 
 // Function to calculate the distance between two locations
 function calculateDistance(loc1, loc2) {
-    const dx = loc1.x - loc2.x;
-    const dy = loc1.y - loc2.y;
-    const dz = loc1.z - loc2.z;
-    return Math.floor(Math.sqrt(dx * dx + dy * dy + dz * dz));
+	    const dx = loc1.x - loc2.x;
+	    const dy = loc1.y - loc2.y;
+	    const dz = loc1.z - loc2.z;
+	    return Math.floor(Math.sqrt(dx * dx + dy * dy + dz * dz));
 }
 
 world.afterEvents.explosion.subscribe((event) => {
@@ -27,12 +27,38 @@ world.afterEvents.explosion.subscribe((event) => {
 	} else {
 		src = `Unknown`;
 	}
-	logEvent(`explosion`, src, `none`, event.dimension.id, impactedBlocks[0]);
+	let loc;
+	if (impactedBlocks.length > 0 ) {
+		loc = impactedBlocks[0];
+	} else if ( event.location ) {
+		loc = event.location;
+	} else if ( event.source.location ) {
+		loc = event.source.location;
+	} else {
+		loc = { x: 0, y: 0, z: 0 };
+	}
+	logEvent(`explosion`, src, `none`, event.dimension.id, loc);
 	players.forEach(player => {
-		const distance = calculateDistance(player.location, impactedBlocks[0]);
+		const distance = calculateDistance(player.location, loc);
 		logEvent(`explosion`, distance, player.name, player.dimension.id, player.location);
 	});
 });
+
+/*system.runInterval(() => {
+for (const player of world.getAllPlayers()) {
+	    player.onScreenDisplay.setTitle("Hello World");
+	    player.onScreenDisplay.updateSubtitle("Welcome to the server!");
+}
+}, 100);
+*/
+system.runInterval(() => {
+	for (const player of world.getAllPlayers()) {
+		const { x, y, z } = player.location;
+		const cor = `${Math.floor(x/16).toString(16).toUpperCase()}, ${Math.floor(y)}, ${Math.floor(z/16).toString(16).toUpperCase()}`;
+		player.onScreenDisplay.setActionBar(cor);
+	}
+}, 20);
+
 
 world.beforeEvents.playerLeave.subscribe(({ player }) => {
 	logEvent(`playerLeave`, `none`, player.name, player.dimension.id, player.location)
@@ -43,27 +69,27 @@ world.afterEvents.itemUse.subscribe(({ source, itemStack }) => {
 	if (typeIds.includes(itemStack.typeId)) {
 		logEvent(`itemUse`, itemStack.typeId, source.name, source.dimension.id, source.location)
 	}
-//logEvent(`itemUse`, itemStack.typeId, source.name, source.dimension.id, source.location)
+	//logEvent(`itemUse`, itemStack.typeId, source.name, source.dimension.id, source.location)
 });
 
 world.afterEvents.playerInteractWithBlock.subscribe(({ player, block }) => {
 	if (typeIds.includes(block.typeId)) {
 		logEvent(`playerInteractWithBlock`, block.typeId, player.name, player.dimension.id, block.location)
 	}	
-//		logEvent(`playerInteractWithBlock`, block.typeId, player.name, player.dimension.id, block.location)
+	//		logEvent(`playerInteractWithBlock`, block.typeId, player.name, player.dimension.id, block.location)
 });
 
 world.afterEvents.playerPlaceBlock.subscribe(({ player, block }) => {
 	if (typeIds.includes(block.typeId)) {
 		logEvent(`playerPlaceBlock`, block.typeId, player.name, player.dimension.id, block.location)
 	}	
-//		logEvent(`playerPlaceBlock`, block.typeId, player.name, player.dimension.id, block.location)
+	//		logEvent(`playerPlaceBlock`, block.typeId, player.name, player.dimension.id, block.location)
 });
 
 world.afterEvents.playerInteractWithEntity.subscribe(({ player, target }) => {
 	if (typeIds.includes(target.typeId)) {
 		logEvent(`playerInteractWithEntity`, target.typeId, player.name, player.dimension.id, target.location)
 	}	
-//		logEvent(`playerInteractWithEntity`, target.typeId, player.name, player.dimension.id, target.location)
+	//		logEvent(`playerInteractWithEntity`, target.typeId, player.name, player.dimension.id, target.location)
 });
 
